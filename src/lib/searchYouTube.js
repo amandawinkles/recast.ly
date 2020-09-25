edccc6ead8157037db77de054b051c3afb966522
Amanda Winkles <amandawinkles@gmail.com>
2020-09-25 12:52:38 -0500

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+    method: 'GET',
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    contentType: 'application/json',
+    data: {
+      q: options.query,
+      maxResults: options.max || 5,
+      key: options.key,
+      videoEmbeddable: true,
+      part: 'snippet',
+      type: 'video'
+    },
+    success: (data) => {
+      console.log('successfully retrieved data from Youtube API', data);
+      callback(data.items);
+    },
+    error: () => {
+      console.log('error getting data from Youtube API');
+    }
+  });
 };
 
 export default searchYouTube;
